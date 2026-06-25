d079f5c7ccc92a50a51172914f2d47b5b7303437
Kuldeep Kolage <kolagekuldeep09@gmail.com>
2026-06-25 23:04:20 +0530

    API Service addded

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
+
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  'http://192.168.49.2:30340/api'
 
 const client = axios.create({
   baseURL: BASE_URL,
